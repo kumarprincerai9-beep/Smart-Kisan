deb4158cc5f49b630e3b571c0300442ef05a2679
Prince Ji Babuaan <kumarprincerai9@gmail.com>
2026-04-08 16:10:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function login() {
     document.getElementById("msg").innerText = "❌ Wrong Details";
   }
 }
-const apiKey = "YOUR_API_KEY";
+const apiKey= "abc123xyz456";
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Dehradun&appid=${apiKey}&units=metric`)
 .then(response => response.json())
@@ -81,3 +81,67 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Dehradun&appid=${apiKey
   document.getElementById("weather").innerText = "Weather load nahi ho raha ❌";
 });
 const apikey =14791e1ce82c2bc20bfd3f0b8c6ccc06
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "abc123xyz456";
+  authDomain: "Smart-kisan.firebaseapp.com",
+  projectId: "Smart-kishan-3c2a0.firebaseapp.com
+};
+
+const app = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDy-GTdxSgxnWMRQ-0BV8WqLHjHz82WXXE",
+  authDomain: "smart-kisan-3c2a0.firebaseapp.com",
+  projectId: "smart-kisan-3c2a0",
+  storageBucket: "smart-kisan-3c2a0.firebasestorage.app",
+  messagingSenderId: "311582271615",
+  appId: "1:311582271615:web:baf886fbdc9317f076774a",
+  measurementId: "G-66XMQMY8WX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+import { getAuth }from "firebase/auth";
+const auth = getAuth();
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log("User created:", userCredential.user);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log("Login success:", userCredential.user);
+  });
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    console.log("Google login:", result.user);
+  });
+import { onAuthStateChanged } from "firebase/auth";
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User logged in:", user.uid);
+  } else {
+    console.log("User logged out");
+  }
+});
